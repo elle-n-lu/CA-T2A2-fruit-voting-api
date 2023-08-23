a9d341c2b5cdd9badfafd44afc48349b517fe3cf
elle-n-lu <joew1960938924@gmail.com>
2023-08-23 18:22:18 +1000

    make it better

--- run.py
+++ run.py
@@ -3,6 +3,16 @@ import os
 from app import setup
 from pkg_init import db, bcrypt
 from models.admin import Admin
+from models.users import User
+from models.cinema import Cinema
+from models.movies import Movie
+from models.comments import Comment
+from models.votes import Vote
+from models.orders import Order
+from models.schedules import Schedule
+from models.sessions import Session
+from models.seats import Seat
+from models.movie_seat import Movie_Seat
 import sqlalchemy as sa
 
 
