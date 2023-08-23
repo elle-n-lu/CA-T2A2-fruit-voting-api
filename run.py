f349f44640d12dbee54386192fa2a9fbb599c4c4
elle-n-lu <joew1960938924@gmail.com>
2023-08-23 18:18:23 +1000

    make it better

--- run.py
+++ run.py
@@ -10,15 +10,15 @@ app = setup()
 
 if __name__ == "__main__":
     with app.app_context():
-        db.drop_all()
-        # engine = sa.create_engine(os.environ.get("DATABASE_URL"))
+        # db.drop_all()
+        engine = sa.create_engine(os.environ.get("DATABASE_URL"))
 
-        # insp = sa.inspect(engine)
-        # for table_entry in reversed(insp.get_sorted_table_and_fkc_names()):
-        #     table_name = table_entry[0]
-        #     if table_name:
-        #         with engine.begin() as conn:
-        #             conn.execute(sa.text(f'DROP TABLE IF EXISTS "{table_name}" CASCADE'))
+        insp = sa.inspect(engine)
+        for table_entry in reversed(insp.get_sorted_table_and_fkc_names()):
+            table_name = table_entry[0]
+            if table_name:
+                with engine.begin() as conn:
+                    conn.execute(sa.text(f'DROP TABLE IF EXISTS "{table_name}" CASCADE'))
         db.create_all()
         admin = User(
             username="admin",
