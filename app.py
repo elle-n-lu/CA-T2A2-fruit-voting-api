0d5a90029df45b9f6cbd052b28a07ca0b1738d0c
elle-n-lu <joew1960938924@gmail.com>
2023-08-24 20:38:07 +1000

    make it better

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def setup():
     
     @app.errorhandler(exc.IntegrityError)
     def default_error(e):
-        flash({'error': e.orig.args[1]})
+        flash({'error': e.orig.args[0]})
         return jsonify({'error': e.description}), 400
     
     @app.errorhandler(BadRequest)
